cd7003968006a0a2524fd473c471d2007f92bc0a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-24 15:17:20 +0300

    Added a whole bunch of markup

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -13,6 +13,14 @@ const Layout = () =>
                     &lt;body&gt;
                 </span>
                 <Outlet />
+
+                <span className="tags bottom-tags">
+                    &lt;body&gt;
+                    <br />
+                    <span className="bottom-tag-html">
+                        &lt;/html&gt;
+                    </span>
+                </span>
             </div>
         </div>
     )
