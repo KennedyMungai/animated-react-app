b62021df6e069cbf93596acb59703b769b1e3736
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-24 14:42:47 +0300

    Added a div with some styling and imported Link from react-router-dom

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './index.scss'
 
 const Sidebar = () =>
 {
     return (
-        <div>Sidebar</div>
+        <div className='nav-bar'>
+        </div>
     )
 }
 
