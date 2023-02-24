334ca018b0748f4df9a3ee430827914c70a6b371
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-24 14:55:46 +0300

    Added the three sidebar items

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,6 +3,8 @@ import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () =>
 {
@@ -13,7 +15,15 @@ const Sidebar = () =>
                 <img className='sub-logo' src={LogoSubtitle} alt='Logo Subtitle' />
             </Link>
             <nav>
-
+                <NavLink exact='true' activeclassname='active' to='/'>
+                    <FontAwesomeIcon icon={faHome} color='4d4d4e' />
+                </NavLink>
+                <NavLink exact='true' activeclassname='active' to='/about' className='about-link'>
+                    <FontAwesomeIcon icon={faUser} color='4d4d4e' />
+                </NavLink>
+                <NavLink exact='true' activeclassname='active' to='/' className='contact-link'>
+                    <FontAwesomeIcon icon={faEnvelope} color='4d4d4e' />
+                </NavLink>
             </nav>
         </div>
     )
