a17bc1ead9d3d0c04bfd7ce0e501001e387ef169
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-24 15:29:02 +0300

    Added some refactoring

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,8 +6,12 @@ const Home = () =>
     return (
         <div className='container home-page'>
             <div className="text-zone">
-                <h1>Hi, <br /> I'm </h1>
-                <img src={LogoTitle} alt="developer" />
+                <h1>Hi, <br /> I'm
+                    <img src={LogoTitle} alt="developer" />
+                    lobodan
+                    <br />
+                    web developer
+                </h1>
             </div>
         </div>
     )
