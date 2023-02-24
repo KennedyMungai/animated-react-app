e7ae8ff5f0a7634a1bad407df92f92386452e54a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-24 14:46:34 +0300

    Added Logo subtitle under it

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
 
 const Sidebar = () =>
 {
@@ -9,6 +10,7 @@ const Sidebar = () =>
         <div className='nav-bar'>
             <Link className='logo' to='/'>
                 <img src={LogoS} alt='Logo S' />
+                <img src={LogoSubtitle} alt='Logo Subtitle' />
             </Link>
         </div>
     )
